7049fc4428b527c457e276916fdd1a31b67fce55
optivoTech <77517141+optivoTech@users.noreply.github.com>
2024-07-19 10:22:36 +0200

    cleanup

--- frontend/src/utils/model/index.ts
+++ frontend/src/utils/model/index.ts
@@ -1,24 +1,6 @@
 import { Remote, wrap } from 'comlink';
 import ComlinkWorker, { EmbeddingExtractionWorker } from './embedding_extractor.worker';
 
-const saveFile = async (data: string) => {
-  const blob = new Blob([data], { type: 'application/json' });
-  const link = document.createElement('a');
-  link.download = 'embeddings.json';
-  link.href = window.URL.createObjectURL(blob);
-  document.body.appendChild(link);
-  link.click();
-  if (link.parentNode) link.parentNode.removeChild(link);
-};
-function sliceIntoChunks(arr: { id: string; content: string }[], chunkSize: number) {
-  const res = [];
-  for (let i = 0; i < arr.length; i += chunkSize) {
-    const chunk = arr.slice(i, i + chunkSize);
-    res.push(chunk);
-  }
-  return res;
-}
-
 export class ModelSearcher {
   private worker: Remote<EmbeddingExtractionWorker> | null;
 
